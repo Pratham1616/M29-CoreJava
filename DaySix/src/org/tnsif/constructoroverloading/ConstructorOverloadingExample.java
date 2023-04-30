6b4c653b860127db03ba2c4d1f7220b0bbd1ae1f
Pratham1616 <prathameshpa@sjcem.edu.in>
2023-04-30 13:13:43 +0530

    Day Six Commit

---
+++ DaySix/src/org/tnsif/constructoroverloading/ConstructorOverloadingExample.java
@@ -0,0 +1,19 @@
+package org.tnsif.constructoroverloading;
+
+public class ConstructorOverloadingExample {
+	@SuppressWarnings("unused")
+	private int x;
+	@SuppressWarnings("unused")
+	private int y;
+	
+	//constructor overloading
+	public ConstructorOverloadingExample() {
+		System.out.println("Default Constructor");
+	}
+	public ConstructorOverloadingExample(int x) {
+		System.out.println("Parameterized Constructor " + x);
+	}
+	public ConstructorOverloadingExample(int x, int y) {
+		System.out.println("Parameterized Constructor " + x + " " + y);
+	}
+}
